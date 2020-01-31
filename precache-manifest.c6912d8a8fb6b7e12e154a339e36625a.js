872684f4caefe5d193f03da021925b0acbed5ccb
bpoupado <brunopoupado@gmail.com>
2020-01-31 19:03:56 +0000

    Updates

---
+++ precache-manifest.c6912d8a8fb6b7e12e154a339e36625a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5457283b0a1fb1ac88a8a87f403cbf",
+    "url": "/monster-list/index.html"
+  },
+  {
+    "revision": "c1c48f55d368ab799c07",
+    "url": "/monster-list/static/css/main.4d7b4e59.chunk.css"
+  },
+  {
+    "revision": "5719c24a9f996d9fe6fe",
+    "url": "/monster-list/static/js/2.37b4ecee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-list/static/js/2.37b4ecee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c48f55d368ab799c07",
+    "url": "/monster-list/static/js/main.c37899bc.chunk.js"
+  },
+  {
+    "revision": "8c252d58b24ec1432e2d",
+    "url": "/monster-list/static/js/runtime-main.21e5966d.js"
+  }
+]);
